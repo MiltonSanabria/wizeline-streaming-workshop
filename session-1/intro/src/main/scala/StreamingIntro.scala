b3f657f2732331957e79d0c55133603ac801cb00
carloslmew <carlos.lm@wizeline.com>
2022-08-22 17:11:37 -0500

    feat: add console and kafka topic to the stream

--- session-1/intro/src/main/scala/StreamingIntro.scala
+++ session-1/intro/src/main/scala/StreamingIntro.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 
 object StreamingIntro {
   def main(args: Array[String]): Unit = {
@@ -8,7 +8,7 @@ object StreamingIntro {
     val spark = SparkSession
       .builder()
       .master("local")
-      .appName("Kafka Source")
+      .appName("Kafka Source Example")
       .getOrCreate()
 
     // Set Spark logging level to ERROR.
@@ -20,6 +20,7 @@ object StreamingIntro {
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "test")
+      .option("failOnDataLoss","false") // Whether to fail the query when it's possible that data is lost (e.g., topics are deleted, or offsets are out of range).
       .load()
       .select(col("value").cast("string"))
 
@@ -28,16 +29,28 @@ object StreamingIntro {
       .groupBy("word")
       .count()
 
-    val result = wordCount.select(to_json(struct($"word", $"count")).alias(("value")))
+    val result = wordCount.select(to_json(struct($"word", $"count")).alias("value"))
 
+    // Save in a Kafka Topic
     result
       .writeStream
       .outputMode(OutputMode.Update())
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("topic", "testConsumer")
+      .option("topic", "testConsumerSpark")
       .option("checkpointLocation", "checkpoint/kafka_checkpoint")
       .start()
-      .awaitTermination()
+
+
+
+    // Print in Console, runs micro-batch as soon as it can
+    result
+       .writeStream
+       .outputMode(OutputMode.Update())
+       .format("console")
+       .option("truncate","false")
+       .trigger(Trigger.ProcessingTime("2 seconds"))
+       .start()
+       .awaitTermination()
   }
 }
